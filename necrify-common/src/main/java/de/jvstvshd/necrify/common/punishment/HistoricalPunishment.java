06923adb4d8a0edf766772662ca52eb512dcf7c7
JvstvsHD <79066214+JvstvsHD@users.noreply.github.com>
2024-12-31 20:22:42 +0100

    Add licenses

--- necrify-common/src/main/java/de/jvstvshd/necrify/common/punishment/HistoricalPunishment.java
+++ necrify-common/src/main/java/de/jvstvshd/necrify/common/punishment/HistoricalPunishment.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of Necrify (formerly Velocity Punishment), a plugin designed to manage player's punishments for the platforms Velocity and partly Paper.
+ * Copyright (C) 2022-2024 JvstvsHD
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package de.jvstvshd.necrify.common.punishment;
 
 import de.jvstvshd.necrify.api.PunishmentException;
