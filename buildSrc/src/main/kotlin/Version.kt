e25523c2a13aacb2b94beb445c826e8ab8415f6c
JvstvsHD <79066214+JvstvsHD@users.noreply.github.com>
2025-01-31 18:35:26 +0100

    Release 1.2.2

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,7 +1,7 @@
 import org.gradle.api.Project
 
 object Version {
-    const val PROJECT_VERSION = "1.2.2-SNAPSHOT"
+    const val PROJECT_VERSION = "1.2.2"
     const val PROJECT_GROUP = "de.jvstvshd.necrify"
 }
 
